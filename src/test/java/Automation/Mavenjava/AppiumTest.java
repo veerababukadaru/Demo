aebabd81edf0f4607f7ae363bccc999854a400b3
veerababu <babukadaru764@gmail.com>
2021-08-12 16:35:43 +0530

    second time changes

--- src/test/java/Automation/Mavenjava/AppiumTest.java
+++ src/test/java/Automation/Mavenjava/AppiumTest.java
@@ -6,9 +6,13 @@ public class AppiumTest {
 	@Test
 	public void jmeter() {
 	System.out.println("Jmeter testing");
+	System.out.println("checking");
+	System.out.println("Testing");
 	}
 	@Test
   public  void mobileTesting() {
-	System.out.println("Mobile Testing");  
+	System.out.println("Mobile Testing"); 
+	System.out.println("finding");
+	System.out.println("correcting");
   }
 }
--- src/test/java/Automation/Mavenjava/SeleniumTest.java
+++ src/test/java/Automation/Mavenjava/SeleniumTest.java
@@ -6,11 +6,15 @@ public class SeleniumTest {
 	
 	@Test
 	public void seleniumTest() {
-	System.out.println("Selenium test");	
+	System.out.println("Selenium test");
+	System.out.println("corrcection");
+	System.out.println("finding issues");
 	}
 	@Test
 	public void guiTest() {
 		System.out.println("GUI Test");
+		System.out.println("target of the task");
+		System.out.println("basic of knowledge");
 	}
 	 
 
